ff79487d9871e16d9900555363cee786a2101392
Emeryx <omergal2003@gmail.com>
2024-10-11 18:18:00 +0300

    Add dirname

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ let express = require('express');
 let app = express();
 console.log('Hello World') // 1
 app.get("/", (req, res) => { // 2, req = request object. res = response object.
-    res.sendFile('/views/index.html')
+    res.sendFile(__dirname + '/views/index.html')
 })
 
 
