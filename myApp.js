68376ba33d1decca3b41db860718078cb4926f5d
Emeryx <omergal2003@gmail.com>
2024-10-11 18:13:12 +0300

    Send HTML file upon GET / request

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ let express = require('express');
 let app = express();
 console.log('Hello World') // 1
 app.get("/", (req, res) => { // 2, req = request object. res = response object.
-    res.send("Hello Express")
+    res.sendFile('/views/index.html')
 })
 
 
