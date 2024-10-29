be4f799d85d341c4fe706c5a27b34c05d7639680
Omer Gal <omergal2003@gmail.com>
2024-10-29 07:29:06 +0000

    Add nodemon dev dependdancy and apply a middleware function that gives access to the static assets in the server

--- myApp.js
+++ myApp.js
@@ -4,11 +4,7 @@ console.log('Hello World') // 1
 app.get("/", (req, res) => { // 2, req = request object. res = response object.
     res.sendFile(__dirname + '/views/index.html')
 })
-
-
-
-
-
+app.use('/public', express.static(__dirname + '/public'))
 
 
 
