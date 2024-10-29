e47820f18cb19fcbe3b7efd0fcc9ac5d491b0164
Omer Gal <omergal2003@gmail.com>
2024-10-29 07:52:40 +0000

    Modify the json object response if the MESSAGE_STYLE env is "uppercase"

--- myApp.js
+++ myApp.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 let express = require('express');
 let app = express();
 console.log('Hello World') // 1
@@ -6,7 +7,7 @@ app.get("/", (req, res) => { // 2, req = request object. res = response object.
 })
 app.use('/public', express.static(__dirname + '/public'))
 app.get('/json', (req, res) => {
-    res.json({"message": "Hello json"})
+    res.json({"message": process.env.MESSAGE_STYLE=="uppercase"?"HELLO JSON":"Hello json"})
 })
 
 
